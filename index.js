34065593c7575a3f023de883e70aa67852386f12
mads <47996003+1MaddieX@users.noreply.github.com>
2022-02-13 23:20:26 -0500

    fixed styling issues and added config files to enforce proper styling

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-import Gun from "gun";
-import fetch from "node-fetch";
-import getUrls from "get-urls";
+import Gun from 'gun'
+import fetch from 'node-fetch'
+import getUrls from 'get-urls'
 
 // Suppress extraneous GUN logging
-let cl = console.log;
-console.log = () => {};
+let cl = console.log
+console.log = () => {}
 
 // if gun has no results, fetch them from github & update gun
 async function fetchRelays() {
-  let tmpRelays = [];
+  let tmpRelays = []
   let res = await fetch(
-    "https://raw.githubusercontent.com/wiki/amark/gun/volunteer.dht.md"
-  );
-  let data = await res.text();
-  let urls = getUrls(data);
+    'https://raw.githubusercontent.com/wiki/amark/gun/volunteer.dht.md'
+  )
+  let data = await res.text()
+  let urls = getUrls(data)
 
-  urls = Array.from(urls);
+  urls = Array.from(urls)
   urls.forEach((u) => {
-    let testUrl = new URL(u);
+    let testUrl = new URL(u)
 
-    if (testUrl.pathname === "/gun" && testUrl.pathname.indexOf("~~") === -1) {
-      tmpRelays.push(testUrl.href);
+    if (testUrl.pathname === '/gun' && testUrl.pathname.indexOf('~~') === -1) {
+      tmpRelays.push(testUrl.href)
     }
-  });
+  })
 
-  return tmpRelays;
+  return tmpRelays
 }
 
 const Relays = async () => {
-  let gunRelays = [];
+  let gunRelays = []
 
   let gun = new Gun({
-    peers: ["https://relay.gun.ooo", "https://gunjs.herokuapp.com"],
-    file: "gun-relays",
-  });
+    peers: ['https://relay.gun.ooo', 'https://gunjs.herokuapp.com'],
+    file: 'gun-relays',
+  })
 
   // check gun first
   let results = await gun
-    .get("gun-relays")
-    .get("relays")
+    .get('gun-relays')
+    .get('relays')
     .on((data) => {
       // apparently, don't have to do anything here
     })
-    .then();
+    .then()
 
-  if (results) gunRelays = JSON.parse(results);
+  if (results) gunRelays = JSON.parse(results)
   else {
-    gunRelays = await fetchRelays();
-    gun.get("gun-relays").get("relays").put(JSON.stringify(gunRelays));
+    gunRelays = await fetchRelays()
+    gun.get('gun-relays').get('relays').put(JSON.stringify(gunRelays))
   }
 
   // restore normal console logging
-  console.log = cl;
+  console.log = cl
 
-  return gunRelays;
-};
+  return gunRelays
+}
 
 export const forceListUpdate = async () => {
   let gun = new Gun({
-    peers: ["https://relay.gun.ooo", "https://gunjs.herokuapp.com"],
-    file: "gun-relays",
-  });
+    peers: ['https://relay.gun.ooo', 'https://gunjs.herokuapp.com'],
+    file: 'gun-relays',
+  })
 
-  const newRelays = await fetchRelays();
+  const newRelays = await fetchRelays()
 
-  gun.get("gun-relays").get("relays").put(JSON.stringify(newRelays));
+  gun.get('gun-relays').get('relays').put(JSON.stringify(newRelays))
 
   // restore normal console logging
-  console.log = cl;
+  console.log = cl
 
-  return newRelays;
-};
+  return newRelays
+}
 
-export default Relays;
+export default Relays
